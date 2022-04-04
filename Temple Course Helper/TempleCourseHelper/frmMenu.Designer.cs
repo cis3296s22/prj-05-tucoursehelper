7e8f977560a8e8433e25922ed7c5f9c98abbd1b0
Arthur Kozhevnik <kozhevnik1025@gmail.com>
2022-04-04 12:17:16 -0400

    Added Headless/Link/Checks
    - Added headless operations so that the browser will open but will not be visible to the user ( should be commented for testing)
    
    - Added a badInput function to help check if the textboxes had been filled out and if the size of the course code is appropriate
    
    - Changed the link to the correct link

--- Temple Course Helper/TempleCourseHelper/frmMenu.Designer.cs
+++ Temple Course Helper/TempleCourseHelper/frmMenu.Designer.cs
@@ -36,10 +36,10 @@
             this.btnSearch = new System.Windows.Forms.Button();
             this.lblResults = new System.Windows.Forms.Label();
             this.txtBoxCourse1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtBoxCourse4 = new System.Windows.Forms.TextBox();
+            this.txtBoxCourse3 = new System.Windows.Forms.TextBox();
             this.txtBoxCourse2 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.txtBoxEmail = new System.Windows.Forms.TextBox();
             this.btnSend = new System.Windows.Forms.Button();
             this.lblEmail = new System.Windows.Forms.Label();
             this.SuspendLayout();
@@ -98,6 +98,7 @@
             this.lblResults.Size = new System.Drawing.Size(45, 13);
             this.lblResults.TabIndex = 5;
             this.lblResults.Text = "Results:";
+            this.lblResults.Visible = false;
             // 
             // txtBoxCourse1
             // 
@@ -106,19 +107,19 @@
             this.txtBoxCourse1.Size = new System.Drawing.Size(100, 20);
             this.txtBoxCourse1.TabIndex = 6;
             // 
-            // textBox2
+            // txtBoxCourse4
             // 
-            this.textBox2.Location = new System.Drawing.Point(110, 150);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 7;
+            this.txtBoxCourse4.Location = new System.Drawing.Point(110, 150);
+            this.txtBoxCourse4.Name = "txtBoxCourse4";
+            this.txtBoxCourse4.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxCourse4.TabIndex = 7;
             // 
-            // textBox3
+            // txtBoxCourse3
             // 
-            this.textBox3.Location = new System.Drawing.Point(110, 110);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 8;
+            this.txtBoxCourse3.Location = new System.Drawing.Point(110, 110);
+            this.txtBoxCourse3.Name = "txtBoxCourse3";
+            this.txtBoxCourse3.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxCourse3.TabIndex = 8;
             // 
             // txtBoxCourse2
             // 
@@ -127,12 +128,12 @@
             this.txtBoxCourse2.Size = new System.Drawing.Size(100, 20);
             this.txtBoxCourse2.TabIndex = 9;
             // 
-            // textBox5
+            // txtBoxEmail
             // 
-            this.textBox5.Location = new System.Drawing.Point(83, 317);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 20);
-            this.textBox5.TabIndex = 12;
+            this.txtBoxEmail.Location = new System.Drawing.Point(83, 317);
+            this.txtBoxEmail.Name = "txtBoxEmail";
+            this.txtBoxEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxEmail.TabIndex = 12;
             // 
             // btnSend
             // 
@@ -157,12 +158,12 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.txtBoxEmail);
             this.Controls.Add(this.btnSend);
             this.Controls.Add(this.lblEmail);
             this.Controls.Add(this.txtBoxCourse2);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtBoxCourse3);
+            this.Controls.Add(this.txtBoxCourse4);
             this.Controls.Add(this.txtBoxCourse1);
             this.Controls.Add(this.lblResults);
             this.Controls.Add(this.btnSearch);
@@ -187,10 +188,10 @@
         private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.Label lblResults;
         private System.Windows.Forms.TextBox txtBoxCourse1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtBoxCourse4;
+        private System.Windows.Forms.TextBox txtBoxCourse3;
         private System.Windows.Forms.TextBox txtBoxCourse2;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox txtBoxEmail;
         private System.Windows.Forms.Button btnSend;
         private System.Windows.Forms.Label lblEmail;
     }
