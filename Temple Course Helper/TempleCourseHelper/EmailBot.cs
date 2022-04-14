2e241f2e73a8040fff5cb1bb663aa70164855a19
rrubin1219 <jenrubin12@yahoo.com>
2022-04-14 00:49:57 -0400

    Moved around code in EmailBot

--- Temple Course Helper/TempleCourseHelper/EmailBot.cs
+++ Temple Course Helper/TempleCourseHelper/EmailBot.cs
@@ -7,17 +7,26 @@ namespace TempleCourseHelper
 {
     internal class EmailBot
     {
-           public async Task Main(String toEmail)
+        
+        String subjectContent = "Testing Twilio from TU Course Helper";
+        String plainTextContent = "Test content for project";
+        String htmlContent = "";
+
+
+        public async Task Main(String toEmail)
         {
             var apiKey = Environment.GetEnvironmentVariable("SENDGIRID_API_KEY");
             var client = new SendGridClient(apiKey);
             var from = new EmailAddress("tul52326@temple.edu");
+            
             var to = new EmailAddress(toEmail);
-            var subject = "SendGrid Twilio Test";
-            var plaintext = "Testing";
-            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var subject = subjectContent;
+            var plaintext = plainTextContent;
+            var html = htmlContent;
+
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plaintext, htmlContent);
             var response = await client.SendEmailAsync(msg).ConfigureAwait(false);
+            Console.WriteLine(response.StatusCode);
         }
     }
 }
