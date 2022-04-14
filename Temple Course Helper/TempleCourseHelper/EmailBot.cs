6f8b68798f2c35413c2c55ab09ed5423551d0628
rrubin1219 <jenrubin12@yahoo.com>
2022-04-14 00:39:14 -0400

    Added skeleton to EmailBot

--- Temple Course Helper/TempleCourseHelper/EmailBot.cs
+++ Temple Course Helper/TempleCourseHelper/EmailBot.cs
@@ -1,12 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace TempleCourseHelper
 {
     internal class EmailBot
     {
+        static async Task Main()
+        {
+            var apiKey = Environment.GetEnvironmentVariable("SENDGIRID_API_KEY");
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("tul52326@temple.edu");
+            var to = new EmailAddress("tul52326@gmail.com");
+            var subject = "SendGrid Twilio Test";
+            var plaintext = "Testing";
+            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plaintext, htmlContent);
+            var response = await client.SendEmailAsync(msg).ConfigureAwait(false);
+        }
     }
 }
