04b85f22adec6f21f350973775cf98f9f77d8cf4
robnano <tun17440@temple.edu>
2022-04-24 12:14:21 -0400

    Comments added
    Necessary comments added and a document will be generated.

---
+++ Temple Course Helper/Doc/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['coursedetails_0',['CourseDetails',['../class_temple_course_helper_1_1_course_details.html',1,'TempleCourseHelper']]]
+];
--- Temple Course Helper/TempleCourseHelper/EmailBot.cs
+++ Temple Course Helper/TempleCourseHelper/EmailBot.cs
@@ -5,11 +5,21 @@ using SendGrid.Helpers.Mail;
 
 namespace TempleCourseHelper
 {
-    public  class EmailBot
+
+    /// <summary>  
+    /// Email Bot Class that handles sending the email ot the user. 
+    /// </summary>  
+    public class EmailBot
     {      
         private string subjectContent = "Temple Courses";
         private string htmlContent = "";
 
+        /// <summary>  
+        /// Main class that will send the email to the user when EmailBot is called. 
+        /// </summary>  
+        /// <param name="toEmail">Parameter to specify the address ww will send the email.</param>  
+        /// <param name="info">Parameter to specify the info we are sending to the user.</param>  
+        /// <returns>Successfull Task. </returns>  
         public async Task Main(string toEmail, string info)
         {
 
