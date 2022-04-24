c8e818e5bc3aa8b902db39620ea0fd55005db9f1
rrubin1219 <jenrubin12@yahoo.com>
2022-04-24 13:38:58 -0400

    New API Key

--- Temple Course Helper/TempleCourseHelper/Key.cs
+++ Temple Course Helper/TempleCourseHelper/Key.cs
@@ -12,37 +12,36 @@ namespace TempleCourseHelper
         {
             Dictionary<int, string> letters = new Dictionary<int, string>();
 
-            letters.Add(1, "w");
+            letters.Add(1, "n");
             letters.Add(2, "k");
-            letters.Add(3, "g");
-            letters.Add(4, "x");
-            letters.Add(5, "n");
-            letters.Add(6, "h");
-            letters.Add(7, "p");
-            letters.Add(8, "z");
-            letters.Add(9, "e");
-            letters.Add(10, "b");
-            letters.Add(12, "v");
-            letters.Add(13, "c");
-            letters.Add(14, "o");
-            letters.Add(15, "t");
-            letters.Add(16, "q");
-            letters.Add(17, "S");
+            letters.Add(3, "m");
+            letters.Add(4, "a");
+            letters.Add(5, "v");
+            letters.Add(6, "y");
+            letters.Add(7, "c");
+            letters.Add(8, "j");
+            letters.Add(9, "w");
+            letters.Add(10, "z");
+            letters.Add(12, "e");
+            letters.Add(13, "u");
+            letters.Add(14, "d");
+            letters.Add(15, "g");
+            letters.Add(16, "p");
+            letters.Add(17, "i");
 
-            return letters[17] + letters[3].ToUpper() + ".5DJ" +
-                         letters[12] + letters[17] + letters[17] +
-                         letters[15].ToUpper() + letters[7].ToUpper() +
-                         "RY" + letters[3].ToUpper() + letters[4] +
-                         "3" + letters[16] + letters[3] + letters[2] +
-                         letters[1] + letters[2] + "5" + letters[5] +
-                         "0" + letters[3] + ".G3" + letters[13] + letters[16] + "2X" +
-                         letters[10] + letters[14] + letters[7] + letters[5] +
-                         letters[5] + letters[10] + letters[3] + letters[4].ToUpper() +
-                         letters[9].ToUpper() + "1Ul" + letters[4] + letters[6] +
-                         "f10" + letters[3].ToUpper() + "8K" + letters[6] + letters[8] +
-                         "8I" + letters[5] + letters[12].ToUpper() + "9" + letters[8] +
-                         "O5" + letters[9] + letters[9].ToUpper() + "M" +
-                         letters[15] + "Y0U"; 
+            return "S" + letters[15].ToUpper() + "." + letters[7] +
+                   "RD6" + letters[2] + letters[16] + letters[17] +
+                   letters[5].ToUpper() + "Qz" + letters[15].ToUpper() +
+                   "G6" + letters[14] + letters[13].ToUpper() + "_" +
+                   letters[7].ToUpper() + "On" + letters[2].ToUpper() +
+                   "SA." + letters[14] + "_" + letters[5].ToUpper() + "VF" +
+                   letters[4] + letters[14].ToUpper() + "QJ" +
+                   letters[13] + letters[4] + letters[12] + "eC" + letters[4] +
+                   letters[8].ToUpper() + "7d" + letters[14] + letters[2] +
+                   letters[7] + "ByU" + letters[13] + "7W" + letters[9] +
+                   letters[6] + letters[4].ToUpper() + "QF4S" + letters[5] +
+                   letters[3] + letters[8] + letters[14].ToUpper() + "s" +
+                   letters[15] + letters[10] + "ss";
         }
     }
 }
